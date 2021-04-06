d671060b40a43c949740fe96e823a9e3c5b2870b
jodafons <jodafons@lps.ufrj.br>
2021-04-06 18:16:07 -0300

    update

--- versions/v1/r0/create_tasks.py
+++ versions/v1/r0/create_tasks.py
@@ -6,9 +6,9 @@ path = basepath + '/Zrad/v1/r0'
 
 command = """maestro.py task create \
   -v {PATH} \
-  -t user.jodafons.mc16_13TeV.sgn.medium.gammajet.bkg.vetoloose.dijet.v1_et{ET}_eta{ETA}.r0 \
+  -t user.jodafons.mc16_13TeV.sgn.MC.gammajet.bkg.vetoMC.dijet.v1_et{ET}_eta{ETA}.r0 \
   -c user.jodafons.job_config.Zrad_v1.n2to5.10sorts.10inits.r0 \
-  -d user.jodafons.mc16_13TeV.sgn.medium.gammajet.bkg.vetoloose.dijet_et{ET}_eta{ETA}.npz \
+  -d user.jodafons.mc16_13TeV.sgn.MC.gammajet.bkg.vetoMC.dijet_et{ET}_eta{ETA}.npz \
   --sd "{REF}" \
   --exec "run_tuning.py -c %IN -d %DATA -r %REF -v %OUT -b zrad -t v1 -p r0" \
   --queue "gpu" """
@@ -20,7 +20,7 @@ except:
 
 for et in range(5):
     for eta in range(5):
-        ref = "{'%%REF':'user.jodafons.mc16_13TeV.sgn.medium.gammajet.bkg.vetoloose.dijet_et%d_eta%d.ref.pic.gz'}"%(et,eta)
+        ref = "{'%%REF':'user.jodafons.mc16_13TeV.sgn.MC.gammajet.bkg.vetoMC.dijet_et%d_eta%d.ref.pic.gz'}"%(et,eta)
         cmd = command.format(ET=et,ETA=eta,REF=ref,PATH=path)
         print(cmd)
         os.system(cmd)
