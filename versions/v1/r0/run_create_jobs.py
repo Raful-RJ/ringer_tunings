fc86a4ee0281d1d4aa602e9ec1ffd911e7c69fe4
Rafael Rodrigues Vianna <rafael.vianna@caloba21.lps.ufrj.br>
2021-04-10 15:47:10 -0300

    run_create_jobs

--- versions/v1/r0/run_create_jobs.py
+++ versions/v1/r0/run_create_jobs.py
@@ -1,29 +1,29 @@
 import argparse as ap
-import numpy as np
-from saphyra import *
-from tensorflow.keras.models import Sequential, model_from_json
+from create_jobs import *
+from tensorflow.keras.models import model_from_json
 
 parser =  ap.ArgumentParser(description='Create Jobs parser control for zrad transfer')
 
 parser.add_argument('--oldmodels', metavar = 'Old model files', nargs = '*', dest='list_old_models',
 required = True, help = 'Old model directory path')
 
-array_old_models = np.array([parser.parse_args().list_old_models])
+list_old_models = parser.parse_args().list_old_models
 
-et_min, et_max = 0,4
-eta_min, eta_max = 0,3
+et_min, et_max = 0,0
+eta_min, eta_max = 0,0
 
 for et in range(eta_min,et_max+1):
   for eta in range(eta_min,eta_max+1):
-      phase = 'et_' + str(et) + '_eta' + str(eta)
+        phase = 'et' + str(et) + '_eta' + str(eta)
 
-      std_old_json_model_path = [(phase in e) and ('.json' in e) for e in array_old_models][0]
+        index = [ ( (phase in e) and ('.json' in e) and ('Tight' in e) ) for e in list_old_models ]
+        array_old_models = np.array(list_old_models)
+        std_old_json_model_path = array_old_models[index][0]
+        json_file = open(std_old_json_model_path, 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        model = model_from_json(loaded_model_json)
+        #Loading weigths into model
+        model.load_weights(std_old_json_model_path[:-5] + ".h5", 'r')
 
-      json_file = open(std_old_json_model_path, 'r')
-      loaded_model_json = json_file.read()
-      json_file.close()
-      model = model_from_json(loaded_model_json)
-      #Loading weigths into model
-      model.load_weights(std_old_json_model_path[:-5] + ".h5", 'r')
-
-      print(model.summary())
+        
